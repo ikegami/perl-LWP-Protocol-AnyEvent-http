959a23e271e997b0f1d59c95a136fc449a775f0c
Eric Brine <ikegami@adaelis.com>
2013-08-05 21:42:15 -0400

    Bump to dev version

--- lib/LWP/Protocol/AnyEvent/http.pm
+++ lib/LWP/Protocol/AnyEvent/http.pm
@@ -4,7 +4,7 @@ package LWP::Protocol::AnyEvent::http;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv('v1.8.0');
+use version; our $VERSION = qv('v1.9.0');
 
 use AnyEvent            qw( );
 use AnyEvent::HTTP      qw( http_request );
@@ -199,7 +199,7 @@ LWP::Protocol::AnyEvent::http - Event loop friendly HTTP and HTTPS backend for L
 
 =head1 VERSION
 
-Version 1.8.0
+Version 1.9.0
 
 
 =head1 SYNOPSIS
